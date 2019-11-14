de042288d14886ddb5bc6d5ff341725ae5536b55
hfurtadob <henriquefurtadob@gmail.com>
2019-11-13 22:40:42 -0300

    model e controller user

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,15 @@
+import User from '../models/user'
+
+class UserController {
+  async store(req, res) {
+    const userExist = await User.findOne({ where: { email: req.body.email } })
+    if (userExist) {
+      return res.status(400).json({ error: 'Usuário já cadastrado' })
+    }
+    const user = await User.create(req.body)
+
+    return res.json(user)
+  }
+}
+
+export default new UserController()
