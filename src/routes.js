b2076cd4025ebc2b7003947e20f8be9bc0c6ea08
hfurtadob <henriquefurtadob@gmail.com>
2019-11-14 01:17:40 -0300

    middleware de autenticação

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,13 @@ import { Router } from 'express'
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 
+import authMiddlewares from './app/middlewares/auth'
+
 const routes = new Router()
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+
+routes.use(authMiddlewares)
+routes.put('/users', UserController.update)
 export default routes
